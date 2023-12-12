9b57703fd63d5525a1b44ee901a023d392eb5182
Technerd345 <yeabnoah5@gmail.com>
2023-12-12 15:26:10 +0300

    modified:   Backend/middleware/generateToken.js 	modified:   frontend_mobile

--- Backend/middleware/generateToken.js
+++ Backend/middleware/generateToken.js
@@ -5,7 +5,7 @@ const generateToken = (userID) => {
   const payload = { userId: userID }; // Create a plain object with the required payload
 
   const token = jwt.sign(payload, process.env.SECRETJWTKEY, {
-    expiresIn: "1hr",
+    expiresIn: "100hr",
   });
 
   return token;
